57a86755e01936e9e56876d6b91f21598939e79f
Jan S <jan.schreib@gmail.com>
2018-11-14 21:55:22 +0100

    use heroku variable port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 extern crate actix_web;
 
 use actix_web::{server, App, HttpRequest};
@@ -6,9 +8,14 @@ fn index(_req: &HttpRequest) -> &'static str {
     "Hello World"
 }
 
+/// Look up our server port number in PORT, for compatibility with Heroku.
+fn get_server_port() -> String {
+    env::var("PORT").ok().unwrap_or("8080".to_string())
+}
+
 fn main() {
     server::new(|| App::new().resource("/", |r| r.f(index)))
-        .bind("127.0.0.1:8000")
+        .bind(format!("{}:{}", "127.0.0.1", get_server_port()))
         .unwrap()
         .run();
 }
